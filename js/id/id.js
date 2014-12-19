e3d4eb0128eb9d1da116e4130ec4755abf0b2858
Jim McAndrew <jim@loc8.us>
2014-12-19 13:10:43 -0700

    Change IP address

--- js/id/id.js
+++ js/id/id.js
@@ -5,9 +5,9 @@ window.iD = function () {
     iD.npmap = {
         settings: {
             connection: {
-              api: 'http://165.83.18.96:8000',
+              api: 'http://165.83.18.141:8000',
               oauth: {
-                url: 'http://165.83.18.96:8000',
+                url: 'http://165.83.18.141:8000',
                 consumerKey: 'CpIont3biEafgafInTYWkFlooQkcFLtGREu6yMG0',
                 secret: 'MFgSWe00v8EsddR9KI42uZZX61r2XL8JwEPxHY2p'
               }
