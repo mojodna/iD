e859c486e431648b36e31b3bae0af29c6f56c038
Jim McAndrew <jim@loc8.us>
2015-01-13 09:27:51 -0700

    Add the npmap install script to the package

--- js/id/npmap.js
+++ js/id/npmap.js
@@ -1,1 +1,46 @@
-iD.npmap = {settings: ''};
+iD.npmap = {
+    "settings": {
+        "connection": {
+            "api": "http://localhost",
+            "oauth": {
+                "url": "http://localhost",
+                "consumerKey": "consumer_key",
+                "secret": "consumer_secret"
+            }
+        },
+        "editing": {
+            "area": true,
+            "disabledFields": [
+                "nps:places_uuid",
+                "nps:building_id",
+                "nps:fcat"
+            ],
+            "line": false,
+            "minZoom": 15,
+            "point": true
+        },
+        "locationOverlayMaxZoom": 22,
+        "map": {
+            "center": [
+                -77.0228,
+                38.8944
+            ],
+            "defaultBackground": "Esri",
+            "zoom": 15.01
+        },
+        "tags": {
+            "disabledFields": [
+                "nps:building_id",
+                "nps:fcat",
+                "nps:places_id"
+            ],
+            "uninterestingFields": [
+                "attribution",
+                "created_by",
+                "nps:unit_code",
+                "odbl",
+                "source"
+            ]
+        }
+    }
+}
