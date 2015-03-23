86c8fb97b46132af03afba64ce12c2e5c17e8191
Nate Irwin <nate@nateirwin.net>
2015-03-23 09:55:41 -0600

    Trying to revert NPMaki back

--- js/id/npmap.js
+++ js/id/npmap.js
@@ -1,11 +1,11 @@
 iD.npmap = {
     settings: {
         connection: {
-            api: 'http://localhost',
-            oauth: {
-                url: 'http://localhost',
-                consumerKey: 'consumer_key',
-                secret: 'consumer_secret'
+            "api": "http://10.147.153.193",
+            "oauth": {
+                "url": "http://10.147.153.193",
+                "consumerKey": "CpIont3biEafgafInTYWkFlooQkcFLtGREu6yMG0",
+                "secret": "MFgSWe00v8EsddR9KI42uZZX61r2XL8JwEPxHY2p"
             }
         },
         editing: {
