824cd1a5ad7cbc4b1238aab7282185d1a10baa9f
Jim McAndrew <jim@loc8.us>
2015-04-10 14:32:25 -0600

    Swap x and y

--- dist/imagery.js
+++ dist/imagery.js
@@ -4970,7 +4970,7 @@ iD.data.imagery = [
             0,
             17
         ],
-        "template": "http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{zoom}/{x}/{y}.png",
+        "template": "http://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{zoom}/{y}/{x}.png",
         "type": "tms"
     }, 
     {
